6ec3fdc6ab004957d3b1fbe96d88be907d5e9783
Martin Møller <mmj@dbc.dk>
2015-03-26 15:33:13 +0100

    added route for rank

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-// GET search page.
-router.get(['/admin', '/admin/*'], (req, res) => {
- res.render('admin');
-});
-
 router.get(['/'], (req, res) => {
   res.render('frontpage');
 });
 
+
+router.get(['/admin', '/admin/*'], (req, res) => {
+ res.render('admin');
+});
+
+// GET search page.
+router.get(['/rank', '/rank/*'], (req, res) => {
+  res.render('rank');
+});
+
 // Fallback route : redirect non-targeted routes to /search/
-router.get('*', (req, res) => res.redirect('/admin/'));
+router.get('*', (req, res) => res.redirect('/'));
 
 module.exports = router;
