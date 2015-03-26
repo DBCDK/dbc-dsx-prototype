e11e2cf62ebdee948e69ad7d7df90d0b45c693ad
Martin Møller <mmj@dbc.dk>
2015-03-26 15:44:44 +0100

    added RankProfilePage

---
+++ client/components/RankProfilePage.react.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var Reflux = require('reflux');
+
+var RankProfile = React.createClass({
+  render: function(){
+    return (
+      <h1>Hest!</h1>
+    );
+  }
+});
+
+module.exports = RankProfile;
