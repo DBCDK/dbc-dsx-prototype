eead7ded0152701a8fadb1dd4fd024d71ac51374
Martin Møller <mmj@dbc.dk>
2015-03-30 16:10:17 +0200

    codestyle

--- client/stores/Dsx.store.js
+++ client/stores/Dsx.store.js
@@ -3,69 +3,71 @@ var actions = require('../actions/Actions.js');
 var socket = require('socket.io-client').connect();
 
 var _store = {
-  selected : [],
-  search: {
-  },
+  selected: [],
+  search: {},
   recommendation: {},
   rank: {}
-}
+};
 
 var local = {
-  get: function () {
+  get: function() {
     var stored = localStorage.getItem('dsxStoreState');
     stored && JSON.parse(stored);
     _store = stored && JSON.parse(stored) || _store;
   },
-  set: function () {
+  set: function() {
     var string = JSON.stringify(_store);
     localStorage.setItem('dsxStoreState', string);
   }
-}
+};
 
 local.get();
-
 
 var DsxStore = reflux.createStore({
   getState: function() {
     return _store;
   },
   search: function(query) {
-    socket.emit('dsxSearchRequest', {query : query.split(' ')});
+    console.log('search');
+    socket.emit('dsxSearchRequest', {query: query.split(' ')});
     _store.search.pending = true;
     _store.search.query = query;
     _store.search.result = null;
     this.pushStore();
   },
-  getRecommendations: function (ids) {
-    socket.emit('dsxRecommendRequest', {query : ids});
+  getRecommendations: function(ids) {
+    console.log('getRecommendations');
+    socket.emit('dsxRecommendRequest', {query: ids});
     _store.search.pending = true;
     this.pushStore();
   },
-  select: function (element) {
+  select: function(element) {
     _store.selected.push(element);
     this.pushStore();
   },
-  unselect: function (element) {
+  unselect: function(element) {
     _store.selected = _store.selected.filter((item) => item.id !== element.id);
     this.pushStore();
   },
-  setRecommendations: function (elements) {
+  setRecommendations: function(elements) {
     _store.selected = elements;
     this.pushStore();
   },
-  clear: function (type) {
+  clear: function(type) {
     _store[type] = Array.isArray(_store[type]) ? [] : {};
     this.pushStore();
   },
-  result: function (result) {
+  result: function(result) {
     console.log(result);
     _store.search.pending = false;
     _store.search.result = result.collections;
     this.pushStore();
   },
-  rank: function (query, list) {
+  rank: function(query, list) {
+    console.log('rank');
     _store.search.pending = true;
     _store.search.result = [];
+    console.log(list);
     this.pushStore();
     socket.emit('dsxRankSearchRequest', {
       query: query.split(' '),
@@ -85,7 +87,7 @@ var DsxStore = reflux.createStore({
     socket.on('dsxRankSearchResponse', this.result);
 
   },
-  pushStore: function () {
+  pushStore: function() {
     this.trigger(_store);
     local.set();
   }
