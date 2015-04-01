d102d44e069c779e3f8448acef7a491de7eb796c
Martin Møller <mmj@dbc.dk>
2015-04-01 13:54:15 +0200

    added new dummy questions

--- client/stores/Questions.store.js
+++ client/stores/Questions.store.js
@@ -5,7 +5,7 @@ var Socket = require('socket.io-client').connect();
 
 var _store = {
   questions: [
-    {
+    /*{
       pid: '870970-basis:24490521',
       title: 'Min kamp',
       creators: ['Karl Ove Knausgård'],
@@ -25,28 +25,187 @@ var _store = {
       creators: ['Hergé'],
       covers: [],
       pending: true
+    },*/
+    {
+      "id": "870970-basis:44839393",
+      "title": "1984",
+      "creator": [
+        "orwell-george"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:24908747",
+      "title": "dyrekirkegården",
+      "creator": [
+        "king-stephen"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:22757636",
+      "title": "fermats-store-sætning",
+      "creator": [
+        "singh-simon"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:45275388",
+      "title": "fifty-shades-of-grey",
+      "creator": [
+        "james-e-l-"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:07352409",
+      "title": "frøken-smillas-fornemmelse-for-sne",
+      "creator": [
+        "høeg-peter"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:26285240",
+      "title": "harry-potter-og-halvblodsprinsen",
+      "creator": [
+        "rowling-joanne-k-"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:28282745",
+      "title": "havets-katedral",
+      "creator": [
+        "falcones-ildefonso"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:26752418",
+      "title": "pippi-langstrømpe",
+      "creator": [
+        "lindgren-astrid"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:04656504",
+      "title": "rosens-navn",
+      "creator": [
+        "eco-umberto"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:27408958",
+      "title": "alkymisten",
+      "creator": [
+        "coelho-paulo"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:22179969",
+      "title": "bravo-to-nul",
+      "creator": [
+        "mcnab-andy"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:25094042",
+      "title": "da-vinci-mysteriet",
+      "creator": [
+        "brown-dan"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:27322697",
+      "title": "fasandræberne",
+      "creator": [
+        "adler-olsen-jussi"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:22340034",
+      "title": "hobbitten",
+      "creator": [
+        "tolkien-j-r-r-"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:50787540",
+      "title": "michael-laudrup-en-gentleman-i-nye-klæder",
+      "creator": [
+        "jonas-nyrup"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:28237774",
+      "title": "norwegian-wood",
+      "creator": [
+        "murakami-haruki"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:27936121",
+      "title": "peter-pedal-på-himmelfart",
+      "creator": [
+        "rey-h-a-"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:27223184",
+      "title": "pigen-der-legede-med-ilden",
+      "creator": [
+        "larsson-stieg"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:04581296",
+      "title": "staten",
+      "creator": [
+        "platon"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:27088988",
+      "title": "vejen",
+      "creator": [
+        "maccarthy-mccarthy-cormac"
+      ],
+      "abstract": null,
+      "selected": true
     }
-    /*{
-      pid: '870970-basis:51569253',
-      title: 'Min kamp',
-      creators: ['Karl Ove Knausgård'],
-      covers: [],
-      pending: true
-    },
-    {
-      pid: '870970-basis:228750978',
-      title: 'Møgkællinger',
-      creators: ['Gretelise Holm'],
-      covers: [],
-      pending: true
-    },
-    {
-      pid: '870970-basis:01264788',
-      title: 'Månen tur-retur',
-      creators: ['Hergé'],
-      covers: [],
-      pending: true
-    }*/
   ],
   currentQuestion: 0
 };
@@ -62,11 +221,13 @@ var QuestionsStore = Reflux.createStore({
 
   _addCoverUrl: function(result) {
     "use strict";
+    console.log('add');
     var that = this;
     var pid = result.pid;
     var images = result.images;
+    console.log(images);
     _store.questions.forEach(function(item) {
-      if(item.pid == pid) {
+      if(item.id == pid) {
         item.covers = images;
         item.pending = false;
         that.trigger(_store);
@@ -80,7 +241,7 @@ function enrichQuestionData(callback) {
   _store.questions.forEach(function(item) {
     if(!item.cover) {
       var params = {
-        pid: item.pid
+        pid: item.id
       };
       Socket.emit('getImagesRequest', params);
     }
