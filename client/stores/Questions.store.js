aff7884c94c8294a5e2f0a9a2a4630977db7e6da
Martin Møller <mmj@dbc.dk>
2015-04-08 09:52:19 +0200

    - basic object added - styles updated

--- client/stores/Questions.store.js
+++ client/stores/Questions.store.js
@@ -1,207 +1,122 @@
 var Reflux = require('reflux');
 var Actions = require('../actions/Actions');
 var Socket = require('socket.io-client').connect();
-//var data = require('');
 
 var _store = {
   questions: [
-    /*{
-      pid: '870970-basis:24490521',
-      title: 'Min kamp',
-      creators: ['Karl Ove Knausgård'],
-      covers: [],
-      pending: true
-    },
     {
-      pid: '870970-basis:23981343',
-      title: 'Møgkællinger',
-      creators: ['Gretelise Holm'],
-      covers: [],
-      pending: true
-    },
-    {
-      pid: '870970-basis:20521104',
-      title: 'Månen tur-retur',
-      creators: ['Hergé'],
-      covers: [],
-      pending: true
-    },*/
-    {
-      "id": "870970-basis:44839393",
-      "title": "1984",
+      "id": "870970-basis:28315341",
+      "title": "tusmørke",
       "creator": [
-        "orwell-george"
+        "meyer-stephenie"
       ],
       "abstract": null,
       "selected": true
     },
     {
-      "id": "870970-basis:24908747",
-      "title": "dyrekirkegården",
+      "id": "870970-basis:28844891",
+      "title": "mount-københavn",
       "creator": [
-        "king-stephen"
+        "colling-nielsen-kaspar"
       ],
       "abstract": null,
       "selected": true
     },
     {
-      "id": "870970-basis:22757636",
-      "title": "fermats-store-sætning",
+      "id": "870970-basis:29541256",
+      "title": "førsteelskeren",
       "creator": [
-        "singh-simon"
+        "skov-leonora-christina"
       ],
       "abstract": null,
       "selected": true
     },
     {
-      "id": "870970-basis:45275388",
-      "title": "fifty-shades-of-grey",
+      "id": "870970-basis:25993055",
+      "title": "filosofi-i-terrorens-tid",
       "creator": [
-        "james-e-l-"
+        "jürgen-habermas"
       ],
       "abstract": null,
       "selected": true
     },
     {
-      "id": "870970-basis:07352409",
-      "title": "frøken-smillas-fornemmelse-for-sne",
+      "id": "870970-basis:29648530",
+      "title": "messi",
       "creator": [
-        "høeg-peter"
+        "faccio-leonardo"
       ],
       "abstract": null,
       "selected": true
     },
     {
-      "id": "870970-basis:26285240",
-      "title": "harry-potter-og-halvblodsprinsen",
+      "id": "870970-basis:50715272",
+      "title": "jeg-er-zlatan",
       "creator": [
-        "rowling-joanne-k-"
+        "david-lagercrantz"
       ],
       "abstract": null,
       "selected": true
     },
     {
-      "id": "870970-basis:28282745",
-      "title": "havets-katedral",
+      "id": "870970-basis:26212871",
+      "title": "isprinsessen",
       "creator": [
-        "falcones-ildefonso"
+        "läckberg-camilla"
       ],
       "abstract": null,
       "selected": true
     },
     {
-      "id": "870970-basis:26752418",
-      "title": "pippi-langstrømpe",
+      "id": "870970-basis:24594688",
+      "title": "fluefiskeri",
       "creator": [
-        "lindgren-astrid"
+        "jensen-michael"
       ],
       "abstract": null,
       "selected": true
     },
     {
-      "id": "870970-basis:04656504",
-      "title": "rosens-navn",
+      "id": "870970-basis:28935951",
+      "title": "trafikkens-forurening",
       "creator": [
-        "eco-umberto"
+        "kåre-press-kristensen"
       ],
       "abstract": null,
       "selected": true
     },
     {
-      "id": "870970-basis:27408958",
-      "title": "alkymisten",
+      "id": "870970-basis:26643325",
+      "title": "spil-skak",
       "creator": [
-        "coelho-paulo"
+        "novrup-svend"
       ],
       "abstract": null,
       "selected": true
     },
     {
-      "id": "870970-basis:22179969",
-      "title": "bravo-to-nul",
+      "id": "870970-basis:25486706",
+      "title": "kost-og-graviditet",
       "creator": [
-        "mcnab-andy"
+        "langer-jerk-w-"
       ],
       "abstract": null,
       "selected": true
     },
     {
-      "id": "870970-basis:25094042",
-      "title": "da-vinci-mysteriet",
+      "id": "870970-basis:26500990",
+      "title": "overgangsalder",
       "creator": [
-        "brown-dan"
+        "langer-jerk-w-"
       ],
       "abstract": null,
       "selected": true
     },
     {
-      "id": "870970-basis:27322697",
-      "title": "fasandræberne",
+      "id": "870970-basis:50784460",
+      "title": "slank-med-low-carb",
       "creator": [
-        "adler-olsen-jussi"
-      ],
-      "abstract": null,
-      "selected": true
-    },
-    {
-      "id": "870970-basis:22340034",
-      "title": "hobbitten",
-      "creator": [
-        "tolkien-j-r-r-"
-      ],
-      "abstract": null,
-      "selected": true
-    },
-    {
-      "id": "870970-basis:50787540",
-      "title": "michael-laudrup-en-gentleman-i-nye-klæder",
-      "creator": [
-        "jonas-nyrup"
-      ],
-      "abstract": null,
-      "selected": true
-    },
-    {
-      "id": "870970-basis:28237774",
-      "title": "norwegian-wood",
-      "creator": [
-        "murakami-haruki"
-      ],
-      "abstract": null,
-      "selected": true
-    },
-    {
-      "id": "870970-basis:27936121",
-      "title": "peter-pedal-på-himmelfart",
-      "creator": [
-        "rey-h-a-"
-      ],
-      "abstract": null,
-      "selected": true
-    },
-    {
-      "id": "870970-basis:27223184",
-      "title": "pigen-der-legede-med-ilden",
-      "creator": [
-        "larsson-stieg"
-      ],
-      "abstract": null,
-      "selected": true
-    },
-    {
-      "id": "870970-basis:04581296",
-      "title": "staten",
-      "creator": [
-        "platon"
-      ],
-      "abstract": null,
-      "selected": true
-    },
-    {
-      "id": "870970-basis:27088988",
-      "title": "vejen",
-      "creator": [
-        "maccarthy-mccarthy-cormac"
+        "helena-møller"
       ],
       "abstract": null,
       "selected": true
