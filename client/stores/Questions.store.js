859042b42e8c90259662f4b04275e2cd2e4ded3b
Martin Møller <mmj@dbc.dk>
2015-04-09 21:02:58 +0200

    updated question object

--- client/stores/Questions.store.js
+++ client/stores/Questions.store.js
@@ -121,6 +121,413 @@ var _store = {
       ],
       "abstract": null,
       "selected": true
+    },
+    {
+      "id": "870970-basis:24908747",
+      "title": "dyrekirkegården",
+      "creator": ["king-stephen"],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:22757636",
+      "title": "fermats-store-sætning",
+      "creator": ["singh-simon"],
+      "abstract": null,
+      "selected": true
+    },
+
+    {
+      "id": "870970-basis:29056714",
+      "title": "elefantpassernes-børn",
+      "creator": ["høeg-peter"],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:26285240",
+      "title": "harry-potter-og-halvblodsprinsen",
+      "creator": ["rowling-joanne-k-"],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:28282745",
+      "title": "havets-katedral",
+      "creator": [
+        "falcones-ildefonso"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:28822391",
+      "title": "pippi-langstrømpe-flytter-ind-i-villa-villekulla-og-andre-historier",
+      "creator": [
+        "lindgren-astrid"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:27408958",
+      "title": "alkymisten",
+      "creator": [
+        "coelho-paulo"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:23740338",
+      "title": "kodeord-firewall",
+      "creator": [
+        "macnab-mcnab-andy"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:28768656",
+      "title": "det-forsvundne-tegn",
+      "creator": [
+        "brown-dan"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:50787540",
+      "title": "michael-laudrup-en-gentleman-i-nye-klæder",
+      "creator": [
+        "jonas-nyrup"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:28237774",
+      "title": "norwegian-wood",
+      "creator": [
+        "murakami-haruki"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:27567010",
+      "title": "de-bedste-historier-om-peter-pedal",
+      "creator": [
+        "rey-margret"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:27598420",
+      "title": "mænd-der-hader-kvinder",
+      "creator": [
+        "larsson-stieg"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:23667495",
+      "title": "faidon",
+      "creator": [
+        "platon"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:27088988",
+      "title": "vejen",
+      "creator": [
+        "maccarthy-mccarthy-cormac"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:26738016",
+      "title": "drivhusdrømme",
+      "creator": [
+        "dalby-claus"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:50801241",
+      "title": "den-hvide-have",
+      "creator": [
+        "dalby-claus-pht"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:29022658",
+      "title": "blodig-genvej",
+      "creator": [
+        "hastrup-julie"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:25612833",
+      "title": "lækker-mad-på-30-minutter",
+      "creator": [
+        "christiansen-bo"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:23017253",
+      "title": "lækker-mad-til-diabetikere",
+      "creator": [
+        "monika-donath"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:25230922",
+      "title": "ainsley-harriotts-grillbibel",
+      "creator": [
+        "harriott-ainsley"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:28536666",
+      "title": "hævnens-gudinde",
+      "creator": [
+        "blædel-sara"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:26731437",
+      "title": "sussi-leo-på-den-2-side",
+      "creator": [
+        "leo-brinkmann"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:27276938",
+      "title": "identitet",
+      "creator": [
+        "brinkmann-svend"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:29496161",
+      "title": "giganternes-fald",
+      "creator": [
+        "follett-ken"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:29531714",
+      "title": "at-leve-at-tænke-at-se",
+      "creator": [
+        "hustvedt-siri"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:28455275",
+      "title": "fucking-flink",
+      "creator": [
+        "ap-lars"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:50681580",
+      "title": "kaptajn-dinesen",
+      "creator": [
+        "buk-swienty-tom-oth"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:51087100",
+      "title": "jeg-lægger-dine-breve-under-madrassen",
+      "creator": [
+        "sara-schwardt"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:27443885",
+      "title": "varm-weekend",
+      "creator": [
+        "joan-ørting"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:27992595",
+      "title": "prøverummet",
+      "creator": [
+        "garbers-lotte"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:44820005",
+      "title": "cykling-con-amore",
+      "creator": [
+        "sørensen-rolf"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:51057376",
+      "title": "mtb",
+      "creator": [
+        "svendsen-søren-pht"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:29071241",
+      "title": "mænd-biler",
+      "creator": [
+        "grau-christian"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:24482561",
+      "title": "italienske-motorcykler",
+      "creator": [
+        "poulsen-villy"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:28699034",
+      "title": "historien-om-honda-shadow",
+      "creator": [
+        "andersen-poul-helge"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:50961095",
+      "title": "hold-da-helt-fest",
+      "creator": [
+        "hansen-per-helmer"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:28990529",
+      "title": "pileflet",
+      "creator": [
+        "enemark-jane"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:26293642",
+      "title": "fotografering-med-digital-spejlrefleks",
+      "creator": [
+        "schurmann-henrik"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:26629276",
+      "title": "fantastiske-synsbedrag",
+      "creator": [
+        "lars-thomas"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:29475911",
+      "title": "fashionable-living",
+      "creator": [
+        "munthe-naja"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:29144478",
+      "title": "all-you-knit-is-love",
+      "creator": [
+        "haumann-susie"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:27590284",
+      "title": "origami-teknik-og-tradition",
+      "creator": [
+        "dybkjær-hans"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:25704886",
+      "title": "den-kreative-klasse",
+      "creator": [
+        "florida-richard"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:28682328",
+      "title": "konkurrencestaten",
+      "creator": [
+        "pedersen-ove-k-"
+      ],
+      "abstract": null,
+      "selected": true
+    },
+    {
+      "id": "870970-basis:29269815",
+      "title": "den-utrolige-historie-om-den-kæmpestore-pære",
+      "creator": [
+        "strid-jakob-martin-drm"
+      ],
+      "abstract": null,
+      "selected": true
     }
   ],
   currentQuestion: 0
